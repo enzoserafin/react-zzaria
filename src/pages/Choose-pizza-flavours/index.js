411e6cad75e55f84ecf6e074b5f2c213354515a7
enzo <enzoserafin@gmail.com>
2021-07-02 12:03:10 -0300

    page flavours

--- src/pages/Choose-pizza-flavours/index.js
+++ src/pages/Choose-pizza-flavours/index.js
@@ -1,10 +1,26 @@
-import { Title } from './styles'
+import { Redirect } from 'react-router-dom'
+import { Content, Title } from './styles'
+import { Grid } from '@material-ui/core'
 import t from 'prop-types'
 
+import singularOrPlural from '../../utils/singularOrPlural'
+import { HOME } from '../../routes'
+
 const ChoosePizzaFlavours = ({ location }) => {
-  console.log(location)
+  if (!location.state) {
+    return <Redirect to={HOME} />
+  }
+  const { flavours } = location.state
   return (
-    < Title > Escolha o sabor da pizza</Title >
+    <>
+      <Content>
+        <Grid container direction='column' alignItems='center'>
+          <Title variant='h4' gutterBottom>
+            Escolha até {location.state.flavours} {singularOrPlural(flavours, 'sabor', 'sabores')}:
+          </Title>
+        </Grid>
+      </Content>
+    </>
   )
 }
 
