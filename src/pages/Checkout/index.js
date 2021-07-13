636a7f1e3bb348740a444d228ac928bfdf2221d7
enzo <enzoserafin@gmail.com>
2021-07-13 09:29:36 -0300

    marcação checkout

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,9 +1,25 @@
-import { Content } from './styles'
+import { Grid } from '@material-ui/core'
+import { Content, Title, PaperContainer } from './styles'
 
 const Checkout = () => {
   return (
     <Content>
-      <h1>Checkout</h1>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={6}>
+          <Title>Qual endereço para entrega?</Title>
+          <PaperContainer>Endereço para entrega</PaperContainer>
+
+          <Title>Qual o seu telefone?</Title>
+          <PaperContainer>Telefone</PaperContainer>
+
+        </Grid>
+
+        <Grid container item xs={12} md={6} direction='column'>
+          <Title>Informações do seu pedido:</Title>
+          <PaperContainer>Pedido</PaperContainer>
+        </Grid>
+      </Grid>
+
     </Content>
   )
 }
--- src/pages/Checkout/styles.js
+++ src/pages/Checkout/styles.js
@@ -1,6 +1,28 @@
 import styled from 'styled-components'
+import {
+  Paper,
+  Typography
+} from '@material-ui/core'
 
 export const Content = styled.main`
   flex-grow: 1;
   padding: ${({ theme }) => theme.spacing(3)}px;;
+`
+
+export const PaperContainer = styled(Paper)`
+  && {
+    flex-grow: 1;
+    margin-bottom: ${({ theme }) => theme.spacing(5)}px;
+    padding: ${({ theme }) => theme.spacing(2)}px;
+  }
+`
+
+export const Title = styled(Typography).attrs({
+  align: 'center',
+  gutterBottom: true,
+  variant: 'h6'
+})`
+  && {
+    text-align: left;
+  }
 `
