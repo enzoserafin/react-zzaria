7c7d95a1adaef2b38da5a02aaec0c30a37e2d14b
enzo <enzoserafin@gmail.com>
2021-07-15 16:50:04 -0300

    eviar pedido firebase

--- src/contexts/order.js
+++ src/contexts/order.js
@@ -1,6 +1,8 @@
 import { createContext, useState } from 'react'
 import t from 'prop-types'
 import { v4 } from 'uuid'
+import firebase, { db } from '../services/Firebase'
+import useAuth from '../hooks/auth'
 
 const OrderContext = createContext()
 
@@ -9,6 +11,7 @@ const OrderProvider = ({ children }) => {
   const [orderInProgress, setOrderInProgress] = useState(false)
   const [phone, addPhone] = useState('')
   const [address, addAddress] = useState({})
+  const { userInfo } = useAuth()
 
   function addPizzaToOrder(pizza) {
     if (orderInProgress) {
@@ -18,7 +21,26 @@ const OrderProvider = ({ children }) => {
     addPizza([newPizza(pizza)])
   }
 
-  function sendOrder() {
+  async function sendOrder() {
+    console.log('send order')
+
+    try {
+      await db.collection('orders').add({
+        userId: userInfo.user.uid,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        address,
+        phone,
+        pizzas: pizzas.map((pizza) => ({
+          size: pizza.pizzaSize,
+          flavours: pizza.pizzaFlavours,
+          quantity: pizza.quantity
+        }))
+
+      })
+    } catch (e) {
+      console.log('Erro ao salvar pedido:', e)
+    }
+
     setOrderInProgress(false)
   }
 
